21337f4595b21340ad76801c46b9ad3e31fecb22
FastheDeveloper <109018244+FastheDeveloper@users.noreply.github.com>
2023-01-16 15:22:53 +0100

    Update preapproval.js

--- preapproval.js
+++ preapproval.js
@@ -1,26 +1,30 @@
-const express = require('express');
-const app = express();
-const PORT = process.env.PORT || 3030;
+const http = require('http');
 
-app.get('/loan/:loanAmount/:months', (req, res) => {
-    const loanAmount = parseFloat(req.params.loanAmount);
-    const months = parseInt(req.params.months);
-    const monthlyInterestRate = 0.02;
-    const maxMonthlyPayment = 12;
-
-    if (months > maxMonthlyPayment) {
-        return res.status(400).json({ error: 'Error: Maximum monthly payment exceeded' });
+const server = http.createServer((req, res) => {
+    if(req.url.startsWith("/loan")){
+        let params = req.url.split("/");
+        const loanAmount = parseFloat(params[2]);
+        const months = parseInt(params[3]);
+        const monthlyInterestRate = 0.02;
+        const maxMonthlyPayment = 12;
+    
+        if (months > maxMonthlyPayment) {
+            return res.status(400).json({ error: 'Error: Maximum monthly payment exceeded' });
+        }
+    
+        const monthlyPayment = loanAmount * (monthlyInterestRate + (monthlyInterestRate / (Math.pow(1 + monthlyInterestRate, months) - 1)));
+        const totalPayment = monthlyPayment * months;
+    
+        res.writeHead(200, {'Content-Type': 'application/json'});
+        res.end(JSON.stringify({
+            monthlyPayment: monthlyPayment,
+            totalPayment: totalPayment
+        }));
+    }else{
+        res.end("Invalid Request")
     }
-
-    const monthlyPayment = loanAmount * (monthlyInterestRate + (monthlyInterestRate / (Math.pow(1 + monthlyInterestRate, months) - 1)));
-    const totalPayment = monthlyPayment * months;
-
-    res.json({
-        monthlyPayment: monthlyPayment,
-        totalPayment: totalPayment
-    });
 });
 
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}/ `);
+server.listen(3000, () => {
+    console.log('Server running on port 3000');
 });
