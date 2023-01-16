741cf3f2b203938da01a2f5acca0d1a36b25b365
FastheDeveloper <109018244+FastheDeveloper@users.noreply.github.com>
2023-01-16 15:35:11 +0100

    Update preapproval.js

--- preapproval.js
+++ preapproval.js
@@ -1,30 +1,25 @@
-const http = require('http');
+const express = require('express');
+const app = express();
 
-const server = http.createServer((req, res) => {
-    if(req.url.startsWith("/loan")){
-        let params = req.url.split("/");
-        const loanAmount = parseFloat(params[2]);
-        const months = parseInt(params[3]);
-        const monthlyInterestRate = 0.02;
-        const maxMonthlyPayment = 12;
-    
-        if (months > maxMonthlyPayment) {
-            return res.status(400).json({ error: 'Error: Maximum monthly payment exceeded' });
-        }
-    
-        const monthlyPayment = loanAmount * (monthlyInterestRate + (monthlyInterestRate / (Math.pow(1 + monthlyInterestRate, months) - 1)));
-        const totalPayment = monthlyPayment * months;
-    
-        res.writeHead(200, {'Content-Type': 'application/json'});
-        res.end(JSON.stringify({
-            monthlyPayment: monthlyPayment,
-            totalPayment: totalPayment
-        }));
-    }else{
-        res.end("Invalid Request")
+app.get('/loan/:loanAmount/:months', (req, res) => {
+    const loanAmount = parseFloat(req.params.loanAmount);
+    const months = parseInt(req.params.months);
+    const monthlyInterestRate = 0.02;
+    const maxMonthlyPayment = 12;
+
+    if (months > maxMonthlyPayment) {
+        return res.status(400).json({ error: 'Error: Maximum monthly payment exceeded' });
     }
+
+    const monthlyPayment = loanAmount * (monthlyInterestRate + (monthlyInterestRate / (Math.pow(1 + monthlyInterestRate, months) - 1)));
+    const totalPayment = monthlyPayment * months;
+
+    res.json({
+        monthlyPayment: monthlyPayment,
+        totalPayment: totalPayment
+    });
 });
 
-server.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log('Server running on port 3000');
 });
